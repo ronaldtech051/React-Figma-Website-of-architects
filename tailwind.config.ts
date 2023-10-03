5ae4235e53b60987b1b02dfad5ae4bd7f0978043
Ro11ingButler <rollingkai1214@gmail.com>
2023-10-03 17:38:19 +0800

    Add custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,34 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "woa-white": {
+          DEFAULT: "#FFF",
+          100: "#FBFBFB",
+          200: "#F3F3F3",
+          300: "#F2F2F2",
+          400: "#BDBDBD",
+        },
+        "woa-black": {
+          DEFAULT: "#000",
+          900: "#2C2C2C",
+          800: "#333333",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
