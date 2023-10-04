2c03e08d5ce119caa9f12748d7c7c2d71a1f3980
Ro11ingButler <rollingkai1214@gmail.com>
2023-10-04 14:40:18 +0800

    Add mission section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
+import Mission from "@/components/Mission";
 
 export default function Home() {
   return (
     <main className="h-[2000px] w-full bg-white xl:px-[135px]">
       <Hero />
       <About/>
+      <Mission/>
     </main>
   );
 }
---
+++ components/Mission.tsx
@@ -0,0 +1,44 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+export default function Mission() {
+  return (
+    <section className="xl:mt-[120px]">
+      <h2 className="text-[64px] font-light text-woa-white-400">
+        Main Focus/Mission Statement
+      </h2>
+      <div className="grid grid-cols-2 xl:mt-[62px]">
+        <MissionItem
+          missionNumber="1"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur, lectus et facilisis placerat."
+          tight
+        />
+        <MissionItem
+          missionNumber="2"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur, lectus et facilisis placerat, magna mauris porttitor tortor, a auctor est felis ut nisl."
+        />
+      </div>
+    </section>
+  );
+}
+
+function MissionItem({
+  missionNumber,
+  text,
+  tight,
+}: {
+  missionNumber: string;
+  text: string;
+  tight?: boolean;
+}) {
+  return (
+    <p className="flex h-[140px] items-center justify-center gap-[30px]">
+      <span className="text-[200px] font-[900] text-woa-white-300">
+        {missionNumber}
+      </span>
+      <span className={cn({ "xl:w-[279px]": tight }, "text-[20px] leading-9")}>
+        {text}
+      </span>
+    </p>
+  );
+}
