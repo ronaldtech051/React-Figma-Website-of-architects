14eb3307fe3f397a50d0d0bb482d2dc6a8a7cd67
Ro11ingButler <rollingkai1214@gmail.com>
2023-10-04 14:23:15 +0800

    Add about component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import About from "@/components/About";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main className="h-[2000px] w-full bg-white xl:px-[135px]">
       <Hero />
+      <About/>
     </main>
   );
 }
--- components/About.tsx
+++ components/About.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Button from "./ui/Button";
+import Image from "next/image";
+
+import image1 from "@/public/about/image1.jpg"
+import image2 from "@/public/about/image2.jpg"
+import image3 from "@/public/about/image3.jpg"
 
 export default function About() {
   return (
-    <section className="flex bg-woa-white-100 xl:mt-[150px]">
+    <section className="flex bg-woa-white-100 xl:mt-[120px] xl:h-[435px] xl:p-[30px] gap-[30px]">
+      <div className="grid grid-flow-col grid-rows-2 grid-cols-2 gap-[30px] relative">
+        <Image src={image1} width={270} height={265} alt="..."/>
+        <Image src={image3} width={270} height={140}  className="xl:mt-[95px]" alt="..."/>
+        <Image src={image2} width={270} height={345} alt="..."/>
+      </div>
       <div className="w-[400px]">
-        <h2 className="font-light text-woa-white-400 xl:text-[64px]">About</h2>
+        <h2 className="font-light text-woa-white-400 xl:text-[64px]">
+          About
+        </h2>
         <p className="text-base font-light leading-[25px] text-black">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry&apos;s standard dummy text
@@ -14,7 +26,9 @@ export default function About() {
           only five centuries, but also the leap into electronic typesetting,
           remaining essentially unchanged.
         </p>
-        <Button variant="white">READ MORE &rarr;</Button>
+        <Button variant="white" className="xl:mt-[30px]">
+          READ MORE &rarr;
+        </Button>
       </div>
     </section>
   );
