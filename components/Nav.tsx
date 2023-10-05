238164f353d188ded34f02c94a8a72d9ec114bef
Ro11ingButler <rollingkai1214@gmail.com>
2023-10-05 15:06:54 +0800

    Add nav active highlighting

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 import React, { ReactNode } from "react";
 import Image from "next/image";
 import logoBlack from "@/public/logo-black.png";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
 export default function Nav() {
+  const pathname = usePathname();
+
+  function isLinkActive(url: string, pathname: string) {
+    return pathname === url ? true : false;
+  }
   return (
     <nav className="flex h-[102px] w-full items-center bg-white">
       <Image
@@ -12,20 +23,55 @@ export default function Nav() {
         alt="Black logo"
       />
       <ul className="flex gap-[60px] xl:ml-[330px]">
-        <NavListItem>MAIN</NavListItem>
-        <NavListItem>GALLERY</NavListItem>
-        <NavListItem>PROJECTS</NavListItem>
-        <NavListItem>CERTIFICATIONS</NavListItem>
-        <NavListItem>CONTACTS</NavListItem>
+        <NavListItem href="/" active={isLinkActive("/", pathname)}>
+          MAIN
+        </NavListItem>
+        <NavListItem
+          href="/gallery"
+          active={isLinkActive("/gallery", pathname)}
+        >
+          GALLERY
+        </NavListItem>
+        <NavListItem
+          href="/projects"
+          active={isLinkActive("/projects", pathname)}
+        >
+          PROJECTS
+        </NavListItem>
+        <NavListItem
+          href="/certifications"
+          active={isLinkActive("/certifications", pathname)}
+        >
+          CERTIFICATIONS
+        </NavListItem>
+        <NavListItem
+          href="/contacts"
+          active={isLinkActive("/contacts", pathname)}
+        >
+          CONTACTS
+        </NavListItem>
       </ul>
     </nav>
   );
 }
 
-function NavListItem({ children }: { children: ReactNode }) {
+function NavListItem({
+  children,
+  href,
+  active,
+}: {
+  children: ReactNode;
+  href: string;
+  active?: boolean;
+}) {
   return (
-    <li className="border-y border-y-woa-black-700 px-[14px] py-1 text-xs">
-      {children}
+    <li
+      className={cn(
+        { "border-y border-y-woa-black-700": active },
+        "px-[14px] py-1 text-xs",
+      )}
+    >
+      <Link href={href}>{children}</Link>
     </li>
   );
 }
