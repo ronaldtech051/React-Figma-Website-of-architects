4ca0df088a3765ccf4a95a908a64d3b54b906f32
Ro11ingButler <rollingkai1214@gmail.com>
2023-10-05 18:51:12 +0800

    Make PageButtons its own component

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
+import PageButtons from "@/components/ui/PageButtons";
 import PageTitle from "@/components/ui/PageTitle";
-import { cn } from "@/lib/utils";
+import { getPaginationInfo, paginate } from "@/lib/utils";
 import Image from "next/image";
-import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 
 const images = [
@@ -99,30 +99,6 @@ const images = [
   },
 ];
 
-function paginate(array: Array<any>, p: number, perPage: number) {
-  const start = (p - 1) * perPage;
-  const end = start + perPage;
-
-  // Slice the array to get the desired page
-  const paginatedItems = array.slice(start, end);
-
-  return paginatedItems;
-}
-
-function getPaginationInfo(
-  currentPage: number,
-  totalItems: number,
-  perPage: number,
-) {
-  const totalPages = Math.ceil(totalItems / perPage);
-
-  // Calculate previous and next page numbers
-  const prevPage = currentPage > 1 ? currentPage - 1 : null;
-  const nextPage = currentPage < totalPages ? currentPage + 1 : null;
-
-  return { prevPage, nextPage };
-}
-
 export default function Gallery() {
   const searchParams = useSearchParams();
   let p = searchParams.get("p") ?? 1;
@@ -151,35 +127,13 @@ export default function Gallery() {
           })}
         </>
       </div>
-      <div className="mt-[60px] flex gap-6">
-        <p className="flex items-center gap-8 text-[24px]">
-          <span className="text-[#333]">{String(p).padStart(2, "0")}</span>
-          <span className="text-5xl font-extralight text-[#e0e0e0]">/</span>
-          <span className="text-woa-white-400">
-            {String(totalPages).padStart(2, "0")}
-          </span>
-        </p>
-        <Link
-          className={cn(
-            "h-[53px] w-[53px] border border-woa-white-300 bg-white p-[14px]",
-            { "cursor-not-allowed bg-[#f9f9f9]": !prevPage },
-          )}
-          href={prevPage ? `/gallery/?p=${prevPage}` : ""}
-          scroll={false}
-        >
-          &larr;
-        </Link>
-        <Link
-          className={cn(
-            "h-[53px] w-[53px] border border-woa-white-300 bg-white p-[14px]",
-            { "cursor-not-allowed bg-[#f9f9f9]": !nextPage },
-          )}
-          href={nextPage ? `/gallery/?p=${nextPage}` : ""}
-          scroll={false}
-        >
-          &rarr;
-        </Link>
-      </div>
+      <PageButtons
+        nextPage={nextPage}
+        prevPage={prevPage}
+        p={p}
+        totalPages={totalPages}
+        url="gallery"
+      />
     </main>
   );
 }
---
+++ components/ui/PageButtons.tsx
@@ -0,0 +1,49 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import React from "react";
+
+export default function PageButtons({
+  p,
+  totalPages,
+  prevPage,
+  nextPage,
+  url,
+}: {
+  p: number;
+  totalPages: number;
+  prevPage: number | null;
+  nextPage: number | null;
+  url: string;
+}) {
+  return (
+    <div className="mt-[60px] flex gap-6">
+      <p className="flex items-center gap-8 text-[24px]">
+        <span className="text-[#333]">{String(p).padStart(2, "0")}</span>
+        <span className="text-5xl font-extralight text-[#e0e0e0]">/</span>
+        <span className="text-woa-white-400">
+          {String(totalPages).padStart(2, "0")}
+        </span>
+      </p>
+      <Link
+        className={cn(
+          "h-[53px] w-[53px] border border-woa-white-300 bg-white p-[14px]",
+          { "cursor-not-allowed bg-[#f9f9f9]": !prevPage },
+        )}
+        href={prevPage ? `/${url}/?p=${prevPage}` : ""}
+        scroll={false}
+      >
+        &larr;
+      </Link>
+      <Link
+        className={cn(
+          "h-[53px] w-[53px] border border-woa-white-300 bg-white p-[14px]",
+          { "cursor-not-allowed bg-[#f9f9f9]": !nextPage },
+        )}
+        href={nextPage ? `/${url}/?p=${nextPage}` : ""}
+        scroll={false}
+      >
+        &rarr;
+      </Link>
+    </div>
+  );
+}
